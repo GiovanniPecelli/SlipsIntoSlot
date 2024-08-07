615a736bf8e933cf309f95f4f1d2675c2fa2febd
GiovanniPecelli <imgiovannipecelli@protonmail.com>
2024-08-07 19:14:38 +0200

    caricamento file

---
+++ homeScript.js
@@ -0,0 +1,17 @@
+/* Freccia per tornare al top dalla pagina */
+
+/* const backBtn = document.getElementById('backBtn');
+
+backBtn.addEventListener('click', function() {
+    window.scrollTo(0, 0); iporta la pagina all'inizio con un click 
+});
+
+window.addEventListener('scroll', function() {
+    let posY = window.scrollY;
+
+    if(posY > 0) {
+        backBtn.style.display = 'block';  cosi faccio tornare visibile l'elemento nascosto 
+    } else {  prima devo nascoderlo sul css 
+        backBtn.style.display = 'none';
+    }
+}); */
